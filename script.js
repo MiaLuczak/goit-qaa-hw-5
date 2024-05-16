91bc0ecb4b876ee4a80a49ef876a2d631aeaa48f
Mio Łuczak <mioluczak@gmail.com>
2024-05-16 12:25:40 +0200

    Funkcja zwracająca nową tablicę, w której każda liczba jest podwojona

---
+++ script.js
@@ -0,0 +1,5 @@
+// Tablica wyjściowa liczb
+const numbers = [1, 2, 3];
+
+// 1. Funkcja zwracająca nową tablicę, w której każda liczba jest podwojona
+const doubledNumbers = numbers.map((element) => element * 2);
