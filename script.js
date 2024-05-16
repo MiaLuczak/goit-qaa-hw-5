6650a357aedc5dcde1721969564c896c54cc906f
Mio Łuczak <mioluczak@gmail.com>
2024-05-16 13:25:24 +0200

    Funkcja zwracająca tylko imiona

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-// 1. Funkcja zwracająca nową tablicę, w której każda liczba jest podwojona
-const doubledNumbers = (arr) => arr.map((element) => element * 2);
+// // 1. Funkcja zwracająca nową tablicę, w której każda liczba jest podwojona
+// const doubledNumbers = (arr) => arr.map((element) => element * 2);
+
+// // Testowanie funkcji:
+// // Tworzenie tablicy testowej,
+// const arrOfNumbers = [-18, -2.1446, 0, 0.91, (-6 + -3), (7 + 5), 16.1, 20];
+
+// // Wyświetlenie w console oryginalnej tablicy:
+// console.log('Oryginalna tablica:');
+// console.log(arrOfNumbers);
+
+// // Oddzielenie logów
+// console.log('');
+// // Przekazanie tablicy jako argument do funkcji oraz wyświetlenie wyniku w console:
+// console.log('Podwojona tablica:');
+// console.log(doubledNumbers(arrOfNumbers));
+
+// 2. Funkcja zwracająca tylko imiona z przekazanej tablicy, jako nowa tablica
+
+const nameDisplay = (arr) => arr.map(element => element.name);
 
 // Testowanie funkcji:
-// Tworzenie tablicy testowej,
-const arrOfNumbers = [-18, -2.1446, 0, 0.91, (-6 + -3), (7 + 5), 16.1, 20];
+// Tworzenie tablicy testowej (Generowana za pomocą JSONGenerator i przekonwetwanej)
 
-// Wyświetlenie w console oryginalnej tablicy:
-console.log('Oryginalna tablica:');
-console.log(arrOfNumbers);
-
-// Oddzielenie logów
-console.log('');
-// Przekazanie tablicy jako argument do funkcji oraz wyświetlenie wyniku w console:
-console.log('Podwojona tablica:');
-console.log(doubledNumbers(arrOfNumbers));
